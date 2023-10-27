802037fc016d6c2a7d37d9f2bd2bbe43742ab79d
Alice <goodchild40723@gmail.com>
2023-10-27 23:22:01 +0800

    intro css modify

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
-import "./intro.scss"
+import React from "react";
+import "./intro.scss";
+import intro from "../../assets/intro.png";
 
 const Intro = () => {
   return (
-    <div className='intro'>
-      <div className="left">
-        <div className="title">
-          <h2>About</h2>
-          <h3>關於我們</h3>
-          <h3>Boho Daze</h3>
-
+    <div className="intro">
+      <div className="container">
+        <div className="left">
+          <div className="title">
+            <div className="title-top">
+              <h3>About</h3>
+              <h3>關於我們</h3>
+            </div>
+            <div className="title-bottom">
+              <h3>Boho Daze</h3>
+            </div>
+          </div>
+          <div className="text">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus nemo deserunt omnis quas. Assumenda aliquid aperiam
+              iste atque nihil, ea blanditiis voluptas quod saepe sequi libero
+              minus voluptatum, magni repudiandae!Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Temporibus nemo deserunt omnis quas.
+              Assumenda aliquid aperiam iste atque nihil, ea blanditiis voluptas
+              quod saepe sequi libero minus voluptatum, magni
+              repudiandae!vLorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Temporibus nemo deserunt omnis quas. Assumenda aliquid
+              aperiam iste atque nihil, ea blanditiis voluptas quod saepe sequi
+              libero minus voluptatum, magni repudiandae!
+            </p>
+          </div>
         </div>
-        <div className="text">
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus nemo deserunt omnis quas. Assumenda aliquid aperiam iste atque nihil, ea blanditiis voluptas quod saepe sequi libero minus voluptatum, magni repudiandae!</p>
+        <div className="right">
+          <span>Handmade with Love</span>
+          <img src={intro} alt="" />
         </div>
-      </div>
-      <div className="right">
-        <img src="" alt="" />
-        <span></span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
