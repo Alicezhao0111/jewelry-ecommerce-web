52416e6c29d1552ae59a16e1d52b95a2a219133f
Alice <goodchild40723@gmail.com>
2023-10-30 18:02:19 +0800

    artist new version

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import Navbar from '../../components/navbar/Navbar'
-import Intro from '../../components/intro/intro'
-import Bestsellers from '../../components/bestsellers/Bestsellers'
+// import Bestsellers from '../../components/bestsellers/Bestsellers'
 import "./home.scss"
+import Artist from '../../components/artist/Artist'
 
 function Home() {
   return (
     <div className="home">
     <Hero />
-    <Intro />
-    <Bestsellers />
+    <Artist />
+    
+    
     </div>
 
   )
