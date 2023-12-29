7b557c1a81de76a913a8122e02bd6eea24da91a0
Alice <goodchild40723@gmail.com>
2023-12-29 19:25:17 +0800

    form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Form.scss"
 import { Step } from "../step/Step";
+import whiteArrow from "../../assets/whiteArrow.svg";
 
 const Form = () => {
   return (
@@ -20,10 +21,40 @@ const Form = () => {
                     </div>
                 </div>
                 <div className="formTable">
-                    <form action="" method="post">
+                    <form name="accountForm" id="accountForm" action="" method="post">
                         <div className="formField">
                             <label htmlFor="firstName">First name</label>
                             <input type="text" name='firstName' id='firstName'/>
+                        </div>
+                        <div className="formField">
+                            <label htmlFor="lastName">Last name</label>
+                            <input type="text" name='lastName' id='lastName'/>
+                        </div>
+                        <div className="formField">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name='email' id='email'/>
+                        </div>
+                        <div className="formField">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name='password' id='password'/>
+                        </div>
+                        <div className="formField">
+                            <label htmlFor="birthDate">Birthdate</label>
+                            <input type="date" name='birthDate' id='birthDate'/>
+                        </div>
+                        <div className="formCheck">
+                            <input type="checkbox" value="subscribe" name='newsletter' id='newsletter'/>
+                            <label htmlFor="newsletter">Sign up for our newsletter to get surprise discount coupons now and then!</label>
+                        </div>
+                        <div className="formCheck privacy">
+                            <input type="checkbox" value="agree" name='terms' id='terms'/>
+                            <label htmlFor="terms">I agree to the terms and conditions and the privacy policy</label>
+                        </div>
+                        <div className="formBtn">
+                            <button type="submit">
+                                <h3>NEXT STEP</h3>
+                                <img src={whiteArrow} alt="" />
+                            </button>
                         </div>
                     </form>
                 </div>
